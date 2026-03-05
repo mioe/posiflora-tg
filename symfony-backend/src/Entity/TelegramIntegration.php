4cb823321f24089bceb95c5d81733f97bd5618e2
mioe <mioe.boss@gmail.com>
2026-03-05 12:58:14 +0300

    fix: wizard generate

---
+++ symfony-backend/src/Entity/TelegramIntegration.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Trait\InitUuidV7;
+use App\Repository\TelegramIntegrationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TelegramIntegrationRepository::class)]
+#[ORM\UniqueConstraint(columns: ['shop_id'])]
+class TelegramIntegration
+{
+    use InitUuidV7;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false, columnDefinition: "UUID NOT NULL")]
+    private ?Shop $shop = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $botToken = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $chatId = null;
+
+    #[ORM\Column]
+    private ?bool $enabled = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getShop(): ?Shop
+    {
+        return $this->shop;
+    }
+
+    public function setShop(?Shop $shop): static
+    {
+        $this->shop = $shop;
+
+        return $this;
+    }
+
+    public function getBotToken(): ?string
+    {
+        return $this->botToken;
+    }
+
+    public function setBotToken(string $botToken): static
+    {
+        $this->botToken = $botToken;
+
+        return $this;
+    }
+
+    public function getChatId(): ?string
+    {
+        return $this->chatId;
+    }
+
+    public function setChatId(string $chatId): static
+    {
+        $this->chatId = $chatId;
+
+        return $this;
+    }
+
+    public function isEnabled(): ?bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(bool $enabled): static
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
--- symfony-backend/src/Entity/Trait/InitUuidV7.php
+++ symfony-backend/src/Entity/Trait/InitUuidV7.php
@@ -4,12 +4,13 @@ namespace App\Entity\Trait;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Doctrine\UuidV7Generator;
+use Symfony\Bridge\Doctrine\Types\UuidType;
 use Symfony\Component\Uid\UuidV7;
 
 trait InitUuidV7
 {
     #[ORM\Id]
-    #[ORM\Column(columnDefinition: "UUID DEFAULT uuidv7() NOT NULL")]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
     #[ORM\GeneratedValue(strategy: "CUSTOM")]
     #[ORM\CustomIdGenerator(class: UuidV7Generator::class)]
     private UuidV7 $id;
